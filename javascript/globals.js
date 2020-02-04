dbf300be3865cb11759ca717e91268c8fbb88bf5
unknown <lewisjaggi@gmail.com>
2020-02-04 23:14:38 +0100

    https

--- javascript/globals.js
+++ javascript/globals.js
@@ -2,7 +2,7 @@ var mymap = L.map('map').setView([46.81, 8.22], 5);
 var layerGroup = new L.LayerGroup();
 layerGroup.addTo(mymap);
 var info = L.control();
-var url = 'http://panama.internet-box.ch';
+var url = 'https://panama.internet-box.ch';
 beerstyle = [];
 
 var currentCountry;
